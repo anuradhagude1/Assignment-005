00b22364e4276c33bfaa9218966df8d425108aad
anuradhagude1 <114892332+anuradhagude1@users.noreply.github.com>
2022-10-03 08:30:52 +0530

    Add files via upload

---
+++ mysite/polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+
+
+def index(request):
+    res = {
+        'Message':"Hello, World!"
+    }
+    return JsonResponse(res)
